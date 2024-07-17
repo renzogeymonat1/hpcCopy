ab4f4e86f51f7f390ec5c8a87255d71129a6ea85
Renzo <renzo.geymonat@hotmail.com>
2024-07-17 00:14:04 -0300

    fun iter de calculo funcionando

--- code/algotritmo.py
+++ code/algotritmo.py
@@ -7,12 +7,12 @@ def heuristica1():
     data = pre_data.clean_date()
     df_viajes = data['viajes']
     df_paradas = data['paradas']
-    df_paradas_lineas_direc = data['paradas_lineas_direc']
+    data_paradas_lineas_direc = data['paradas_lineas_direc']
 
     # Lista de paradas por cordenadas para medir distancias
-    df_paradas_lineas_direc = data['paradas_lineas_direc']
-    df_paradas_x = df_paradas_lineas_direc[['COD_UBIC_P', 'X']].drop_duplicates()
-    df_paradas_y = df_paradas_lineas_direc[['COD_UBIC_P', 'Y']].drop_duplicates()
+    data_paradas_lineas_direc = data['paradas_lineas_direc']
+    df_paradas_x = data_paradas_lineas_direc[['COD_UBIC_P', 'X']].drop_duplicates()
+    df_paradas_y = data_paradas_lineas_direc[['COD_UBIC_P', 'Y']].drop_duplicates()
     df_paradas_x_sorted = df_paradas_x.sort_values(by='X')
     df_paradas_y_sorted = df_paradas_y.sort_values(by='Y')
     
@@ -38,12 +38,12 @@ def heuristica1():
     #         iter_de_calculo(
     #             cod_ubic_p, linea['DESC_LINEA'], linea['COD_VARIAN'],
     #             hora, df_paradas_x_sorted, df_paradas_y_sorted, 
-    #             df_cant_viajes_hora, df_paradas_lineas_direc
+    #             df_cant_viajes_hora, data_paradas_lineas_direc
     #         )
     iter_de_calculo(
                 4211, '546', 217,
                 8, df_paradas_x_sorted, df_paradas_y_sorted, 
-                df_cant_viajes_hora, df_paradas_lineas_direc
+                df_cant_viajes_hora, data_paradas_lineas_direc
             )
 
 #     df_cant_viajes_hora
@@ -64,23 +64,9 @@ def iter_de_calculo(
         df_paradas_x_sorted: pd.DataFrame, df_paradas_y_sorted: pd.DataFrame,
         df_cant_viajes_hora: pd.DataFrame, data_paradas_lineas_direc: pd.DataFrame
         ):
-    # 1-descarto viajes que no me sirven (descarto franjas)
-    # data = pre_data.clean_date()
-    # data_viajes_iter = data['viajes']
-
-    # Convertir 'fecha_evento' a formato datetime
-    # data_viajes_iter['fecha_evento'] = pd.to_datetime(data_viajes_iter['fecha_evento'])
-
-    # Definir el límite de tiempo (1 hora 40 minutos)
-    # limite_tiempo = timedelta(hours=1, minutes=40)
-
-    # Filtrar los registros
-    # filtro = data_viajes_iter['fecha_evento'].apply(lambda x: x <= fecha_iter + limite_tiempo)
-    # data_viajes_iter = data_viajes_iter[filtro]
-
-    #2-busco paradas siguientes a la que sube
-    # Filtrar los registros que coinciden con desc_linea y cod_var y están después de cod_parada
     
+    #1-busco paradas siguientes a la que sube
+    # Filtrar los registros que coinciden con desc_linea y cod_var y están después de cod_parada
     # Aplica las dos condiciones a las filas posteriores
     data_paradas_lineas_direc_iter = data_paradas_lineas_direc[
         (data_paradas_lineas_direc['COD_VARIAN'] == cod_var)
@@ -97,121 +83,108 @@ def iter_de_calculo(
     ].index[0]
 
     print(index_initial)
-    # Filtra el DataFrame desde la fila que cumple con las tres condiciones
-    filtered_data = data_paradas_lineas_direc_iter.iloc[index_initial + 1:]
+    # Filtra el DataFrame desde la fila que cumple con las dos condiciones (me quedo con las siguientes paradas a la que sube por eso el +1)
+    data_paradas_lineas_direc_iter = data_paradas_lineas_direc_iter.iloc[index_initial + 1:]
 
-    print(filtered_data)
+    #Sentido/direccion del viaje antes de quitar columnas
+    sentidoViaje = data_paradas_lineas_direc_iter.iloc[0]['DESC_VARIA']
+    print(sentidoViaje)
 
+    print(data_paradas_lineas_direc_iter)
     # print(data_paradas_lineas_direc_iter)
     data_paradas_lineas_direc_iter = data_paradas_lineas_direc_iter.drop_duplicates()
-    data_paradas_lineas_direc_iter = data_paradas_lineas_direc_iter[['COD_UBIC_P', 'X', 'Y']] #Me quedo solo con los cod ubicacion parada y las coordenadas.
 
-
+    #Agrego columnas VOLUMEN y PROBABILIDAD para luego hacer los calculos.
     data_paradas_lineas_direc_iter = data_paradas_lineas_direc_iter.assign(VOLUMEN=0)
     data_paradas_lineas_direc_iter = data_paradas_lineas_direc_iter.assign(PROBABILIDAD=0)
-    # #3-para cada una de esas paradas, busco las mas cercanas y lineas que tengan como retorno una parada cercana a la que sube y calculo volumen.
-    # # Definir la distancia máxima en metros
-    # distancia_maxima = 250
 
+    print(data_paradas_lineas_direc_iter)
+    
+    #2-para cada una de esas paradas, busco las mas cercanas y lineas que tengan como retorno una parada cercana a la que sube y calculo volumen.
+    data_paradas_cercanas_a_origen = distanncia_paradas(cod_parada, df_paradas_x_sorted, df_paradas_y_sorted)
+    print(data_paradas_cercanas_a_origen)
 
-    # # Lista para almacenar los resultados
-    # paradas_cercanas = []
+    origen_paradas_cercanas_list = data_paradas_cercanas_a_origen['COD_UBIC_P'].tolist()
+    print(origen_paradas_cercanas_list)
 
+    # Filtrar el tercer DataFrame para obtener las líneas que pasan por las paradas de origen
+    data_lineas_origen = data_paradas_lineas_direc[data_paradas_lineas_direc['COD_UBIC_P'].isin(origen_paradas_cercanas_list)]
+    #Cambio de nombre para columna para el dataframe data_lineas_origen le agrego a '_CERCANA_ORIGEN' a las columnas
+    data_lineas_origen = data_lineas_origen.rename(columns=lambda x: f'{x}_CERCANA_ORIGEN')
+    print(data_lineas_origen)
 
-    # data_paradas_iter = data['paradas']
-    # # Iterar sobre cada fila en data_paradas_lineas_direc_iter
-    # for index, row in data_paradas_lineas_direc_iter.iterrows():
-    #     cod_ubic_p = row['COD_UBIC_P']
-    #     x1, y1 = row['X'], row['Y']
-       
-    #     # Filtrar paradas cercanas en data_paradas_iter
-    #     for _, parada in data_paradas_iter.iterrows():
-    #         x2, y2 = parada['X'], parada['Y']
-           
-    #         # Calcular la distancia euclidiana
-    #         distancia = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-           
-    #         # Comprobar si la distancia es menor que la distancia máxima y que no sea la misma parada
-    #         if distancia < distancia_maxima and parada['COD_UBIC_P'] != cod_ubic_p:
-    #             paradas_cercanas.append(parada)
-               
-    #     # Convertir la lista de paradas cercanas en un DataFrame
-    #     df_paradas_cercanas = pd.DataFrame(paradas_cercanas)
-    #     #busco lienas en sentido inverso (desc_var) al "princial" que tengan las paradas cercanas y la  parada origen en el recorrido y con esto calculamos volumen.
-    #     df_lineas_cercanas_filtrado = df_paradas_cercanas[['DESC_LINEA']].drop_duplicates()
-    #     df_paradas_lineas_direc = data['paradas_lineas_direc']
+    # Iterar sobre cada fila en data_paradas_lineas_direc_iter
+    for index, row in data_paradas_lineas_direc_iter.iterrows(): 
+        #Busco lineas que que pasen entre las cercanas del origen y las cercanas de las paradas siguientes/proximas.
+        cod_parada_siguiente = row['COD_UBIC_P']
+        data_paradas_cercanas_a_siguientes = distanncia_paradas(cod_parada_siguiente, df_paradas_x_sorted, df_paradas_y_sorted)
+        #print(data_paradas_cercanas_a_siguientes)
+        
+        # Obtener listas de COD_UBIC_P de las paradas cercanas.
+        siguientes_paradas_cercanas_list = data_paradas_cercanas_a_siguientes['COD_UBIC_P'].tolist()
 
+        # obteneR las líneas que pasan por las paradas cercanas a las siguientes
+        data_lineas_siguientes = data_paradas_lineas_direc[data_paradas_lineas_direc['COD_UBIC_P'].isin(siguientes_paradas_cercanas_list)]
+        print( data_lineas_siguientes)
 
-    #     # Lista para almacenar los resultados
-    #     lineas_paradas_cercanas = []
+        # Unir los DataFrames data_lineas_origen y data_lineas_siguientes por DESC_LINEA_CERCANA_ORIGEN = DESC_LINEA y COD_VARIAN_CERCANA_ORIGEN = COD_VARIAN
+        data_lineas_regreso = pd.merge(data_lineas_origen, data_lineas_siguientes,
+                                    left_on=['DESC_LINEA_CERCANA_ORIGEN', 'COD_VARIAN_CERCANA_ORIGEN'],
+                                    right_on=['DESC_LINEA', 'COD_VARIAN'],
+                                    how='inner')
 
+        # Seleccionar las columnas específicas
+        data_lineas_regreso = data_lineas_regreso.loc[:, ['COD_UBIC_P_CERCANA_ORIGEN', 'DESC_LINEA_CERCANA_ORIGEN',
+                                                  'COD_VARIAN_CERCANA_ORIGEN', 'DESC_VARIA_CERCANA_ORIGEN', 'COD_UBIC_P', 'DESC_LINEA', 'COD_VARIAN', 'DESC_VARIA']]
 
-    #     # Iterar sobre cada línea en df_lineas_cercanas_filtrado
-    #     for index, linea in df_lineas_cercanas_filtrado.iterrows():
-    #         desc_linea = linea[['DESC_LINEA', 'COD_UBIC_P']]
-           
-    #         # Condición 1: DESC_LINEA = desc_linea, COD_UBIC_P = cod_parada, DESC_VARIA != desc_var
-    #         condicion1 = df_paradas_lineas_direc[
-    #             (df_paradas_lineas_direc['DESC_LINEA'] == desc_linea) &
-    #             (df_paradas_lineas_direc['COD_UBIC_P'] == cod_parada) &
-    #             (df_paradas_lineas_direc['DESC_VARIA'] != desc_var)
-    #         ]
-           
-    #         # Condición 2: DESC_LINEA = desc_linea, COD_UBIC_P = COD_UBIC_P de df_paradas_cercanas, DESC_VARIA != desc_var
-    #         condicion2 = df_paradas_lineas_direc[
-    #             (df_paradas_lineas_direc['DESC_LINEA'] == desc_linea) &
-    #             (df_paradas_lineas_direc['COD_UBIC_P'] == cod_ubic_p) &
-    #             (df_paradas_lineas_direc['DESC_VARIA'] != desc_var)
-    #         ]
-           
-    #         # Verificar si ambas condiciones existen
-    #         if not condicion1.empty and not condicion2.empty:
-    #              lineas_paradas_cercanas.append(desc_linea)
+        # Filtrar los registros donde DESC_VARIA sea distinto de sentidoViaje
+        data_lineas_regreso = data_lineas_regreso[data_lineas_regreso['DESC_VARIA'] != sentidoViaje]
 
+        # Eliminar duplicados del DataFrame filtrado
+        data_lineas_regreso = data_lineas_regreso.drop_duplicates()
 
-    #         #Ahora con esta lineas, busco el volumen de viajes y acumulo en una variable global total viajes regreso, que va a ser el dividendo de los volumenes por parada para hacer el calculo de prob= casos fav / casos posibles.
-    #         #TENGO Q ITERAR SOBRE PARADAS CERCANAS Y ACUMULAR LOS VIAJES = VOLUMEN.
-    #         acc_volumen = 0
-    #         # Iterar sobre cada fila en lineas_paradas_cercanas
-    #         for index, linea_parada_cercana in lineas_paradas_cercanas.iterrows():
-    #             desc_linea = linea_parada_cercana['DESC_LINEA']
-    #             cod_ubic_p = linea_parada_cercana['COD_UBIC_P']
-               
-    #             # Filtrar data_viajes_iter por COD_UBIC_P y DESC_LINEA
-    #             viajes_filtrados = data_viajes_iter[
-    #                 (data_viajes_iter['codigo_parada_origen'] == cod_ubic_p) &
-    #                 (data_viajes_iter['dsc_linea'] == desc_linea)
-    #             ]
-               
-    #             # Acumular el número de viajes en acc_volumen
-    #             acc_volumen += len(viajes_filtrados)
-       
-    #     # Crear una máscara para seleccionar la fila específica
-    #     mascara = (
-    #         (data_paradas_lineas_direc_iter['COD_UBIC_P'] == cod_ubic_p) &
-    #         (data_paradas_lineas_direc_iter['X'] == x1) &
-    #         (data_paradas_lineas_direc_iter['Y'] == y1)
-    #     )
+        # Seleccionar solo las columnas deseadas
+        data_lineas_regreso = data_lineas_regreso[['COD_UBIC_P', 'DESC_LINEA', 'COD_VARIAN', 'DESC_VARIA']]
 
+        # Mostrar el DataFrame resultante
+        print(data_lineas_regreso)
 
-    #     # Asignar el nuevo valor a la columna VOLUMEN para las filas que coincidan con la máscara
-    #     data_paradas_lineas_direc_iter.loc[mascara, 'VOLUMEN'] = acc_volumen                  
+        print(df_cant_viajes_hora)
+        #Ahora que tengo todas las lineas y las paradas cercanas para posibles regresos, cuento viajes para calcular volumen. 
+        acc_volumen = 0
+        for index, row in data_lineas_regreso.iterrows():
+            cod_ubic_p = row['COD_UBIC_P']
+            desc_linea = row['DESC_LINEA']
+            cod_varian = row['COD_VARIAN']
+            row_cant_viajes = df_cant_viajes_hora[
+                (df_cant_viajes_hora['COD_UBIC_P'] == cod_ubic_p) &
+                (df_cant_viajes_hora['DESC_LINEA'] == desc_linea) &
+                (df_cant_viajes_hora['COD_VARIAN'] == cod_varian) &
+                (df_cant_viajes_hora['hora'] == fecha_iter)
+            ]
 
-
-    # volumen_total = data_paradas_lineas_direc_iter['VOLUMEN'].sum()
-    # for index, row in data_paradas_lineas_direc_iter.iterrows():
-    #     data_paradas_lineas_direc_iter.at[index, 'PROBABILIDAD'] = row['VOLUMEN'] / volumen_total
-    # return data_viajes_iter
+            acc_volumen += row_cant_viajes['cant_viajes'].sum()
+        print(acc_volumen)
+        # Actualizar el valor de la columna 'VOLUMEN' para la fila que cumple la condición
+        data_paradas_lineas_direc_iter.loc[
+        data_paradas_lineas_direc_iter['COD_UBIC_P'] == cod_parada_siguiente, 'VOLUMEN'
+        ] = acc_volumen
+    
+    volumen_total = data_paradas_lineas_direc_iter['VOLUMEN'].sum()
+    print(volumen_total)
+    #Calculo de probabilidad para todas las paradas siguientes.
+    data_paradas_lineas_direc_iter['PROBABILIDAD'] = (data_paradas_lineas_direc_iter['VOLUMEN'] / volumen_total) * 100
+    print(data_paradas_lineas_direc_iter)
 
 #calcular la distancia entre paradas
 def distanncia_paradas(cod_parada: int, df_x: pd.DataFrame, df_y: pd.DataFrame):
     # Calcular las paradas mas cercanas a cod_parada
-    max_distance = 200
+    max_distance = 100
     reference_stop_x = df_x[df_x['COD_UBIC_P'] == cod_parada]
     reference_stop_y = df_y[df_y['COD_UBIC_P'] == cod_parada]
 
     if reference_stop_x.empty:
-        print(f"No se encontró la parada con COD_UBIC_P igual a {cod_parada}")
+        #print(f"No se encontró la parada con COD_UBIC_P igual a {cod_parada}")
         return
     parada_referencia_x = reference_stop_x.iloc[0]['X']
     parada_referencia_y = reference_stop_y.iloc[0]['Y']
@@ -226,14 +199,19 @@ def distanncia_paradas(cod_parada: int, df_x: pd.DataFrame, df_y: pd.DataFrame):
     for _, row in df_x.iterrows():
         if abs(row['X'] - parada_referencia_x) < max_distance:
             df_paradas_x = pd.concat([df_paradas_x, pd.DataFrame([row[['COD_UBIC_P', 'X']]])], ignore_index=True)
-            print(df_paradas_x)
+            #print(df_paradas_x)
     
     for _, row in df_y.iterrows():
         if abs(row['Y'] - parada_referencia_y) < max_distance:
            df_paradas_y = pd.concat([df_paradas_y, pd.DataFrame([row[['COD_UBIC_P', 'Y']]])], ignore_index=True)
+  
+    # Combinar los DataFrames resultantes en uno solo
+    df_resultado = pd.merge(df_paradas_x, df_paradas_y, on=['COD_UBIC_P'])
 
-    return pd.merge(df_paradas_x, df_paradas_y, on=['COD_UBIC_P'])
+    # Convertir la columna COD_UBIC_P a enteros
+    df_resultado['COD_UBIC_P'] = df_resultado['COD_UBIC_P'].astype(int)
 
+    return df_resultado
 
 heuristica1()
 
